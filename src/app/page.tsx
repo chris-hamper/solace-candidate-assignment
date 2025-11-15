c570412cc7cd7751b8272a9b4499825b66be1dd4
Christopher M. Hamper <chris@chrishamper.com>
2025-11-15 15:42:28 -0500

    Resolve hydration and missing key issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,25 +61,27 @@ export default function Home() {
       <br />
       <table>
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>City</th>
+            <th>Degree</th>
+            <th>Specialties</th>
+            <th>Years of Experience</th>
+            <th>Phone Number</th>
+          </tr>
         </thead>
         <tbody>
           {filteredAdvocates.map((advocate) => {
             return (
-              <tr>
+              <tr key={advocate.id}>
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
                 <td>{advocate.city}</td>
                 <td>{advocate.degree}</td>
                 <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
+                  {(advocate.specialties as string[]).map((s: string, idx: number) => ( // TODO: better typing on advocate
+                    <div key={idx}>{s}</div>
                   ))}
                 </td>
                 <td>{advocate.yearsOfExperience}</td>
