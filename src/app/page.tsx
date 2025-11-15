3b4e0d724191fbba242bba35baa77e00cd655c84
Christopher M. Hamper <chris@chrishamper.com>
2025-11-15 15:43:20 -0500

    Fix advocate filtering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,12 +27,11 @@ export default function Home() {
     console.log("filtering advocates...");
     const filteredAdvocates = advocates.filter((advocate) => {
       return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
+        advocate.firstName.toLowerCase().includes(searchTerm) ||
+        advocate.lastName.toLowerCase().includes(searchTerm) ||
+        advocate.city.toLowerCase().includes(searchTerm) ||
+        advocate.degree.toLowerCase().includes(searchTerm) ||
+        (advocate.specialties as string[]).some(str => str.toLowerCase().includes(searchTerm)) // TODO: better typing on advocate
       );
     });
 
